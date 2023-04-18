ffb752ddc0003dc2e200f3e674c1cdece206452d
Daniel Reza <daniel.reza1007@gmail.com>
2023-04-18 13:27:36 -0500

    project setup

---
+++ script.js
@@ -0,0 +1,16 @@
+// Drums, Ambience, Pads, Lead, Bass
+
+// Drums
+// boombap, jazzy, sleepy, chillhop, swing
+
+// Ambience
+// rain, trees/leaves, cityscape, water stream, nighttime 
+
+// pads
+// strings, airy synth, vocal pad, arpeggiated pad, 
+
+// lead instruments
+// vibes, flute, guitar, sax, synth, acoustic guitar, piano
+
+// bass
+// synth, upright, electric, low piano
