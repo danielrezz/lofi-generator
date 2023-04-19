4ca25a1a2e996fe31dd920223a41a3a5d20977a0
Daniel Reza <daniel.reza1007@gmail.com>
2023-04-19 11:35:45 -0500

    more progress

--- script.js
+++ script.js
@@ -1,16 +1,55 @@
-// Drums, Ambience, Pads, Lead, Bass
+// idea: implement API for random lofi track
+// idea2: implement freesound API for random foley sound to use
 
-// Drums
-// boombap, jazzy, sleepy, chillhop, swing
+var drumOptions = [
+  'boombap',
+  'jazzy',
+  'sleepy',
+  'chillhop',
+  'Foley sounds only!',
+];
+var ambOptions = [
+  'rain',
+  'windy trees',
+  'cityscape',
+  'water stream',
+  'nighttime',
+];
 
-// Ambience
-// rain, trees/leaves, cityscape, water stream, nighttime 
+var padOptions = ['strings', 'airy synth', 'vocal', 'arppegiated', 'lush'];
 
-// pads
-// strings, airy synth, vocal pad, arpeggiated pad, 
+var leadOptions = [
+  'vibraphone',
+  'flute',
+  'electric guitar',
+  'acoustic guitar',
+  'piano',
+  'synth',
+  'saxophone',
+  'kalimba',
+];
 
-// lead instruments
-// vibes, flute, guitar, sax, synth, acoustic guitar, piano
+var bassOptions = ['synth', 'upright', 'electric', 'low piano'];
 
-// bass
-// synth, upright, electric, low piano
+let options = [drumOptions, ambOptions, padOptions, leadOptions, bassOptions];
+let generatedOptions = [];
+let tempo;
+let button = document.getElementById('generateBtn');
+
+let printArray = function (arr) {
+  if (typeof arr == 'object') {
+    for (var i = 0; i < arr.length; i++) {
+      printArray(arr[i]);
+    }
+  } else console.log(arr);
+};
+
+printArray(options);
+
+// for (let i = 0; i < 9; i++) {
+//     str = str + i;
+//   }
+
+// let drum = drumOptions[Math.floor(Math.random() * drumOptions.length)];
+
+// console.log(drum);
